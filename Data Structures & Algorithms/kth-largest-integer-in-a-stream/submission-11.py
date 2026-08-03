183552bf2a15a01bb8cf4a3552da99bdccc6fd35
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-03 12:22:40 -0400

    Add: kth-largest-integer-in-a-stream - submission-11

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-11.py
@@ -0,0 +1,19 @@
+#max heap??
+#heap is a priority queue
+#heap should only be size k since k does not change
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        self.k = k
+        heapq.heapify(self.heap)
+        #this doenst necessarily make it a heap, so have to heapify again later
+        self.heap = self.heap[-k:]
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        heapq.heapify(self.heap)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
