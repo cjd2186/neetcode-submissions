9bcc550a3a83667b0435608a9b6049b97f0bdb9d
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-05 12:52:21 -0400

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        #dont need to get square root -- uniform operation across all points, just need min
+        minHeap = []
+        #need K smallest distances to origin -- y1,y2 is 0,0
+        for point in points:
+            distance = point[0]**2 + point[1]**2
+            #use negative distance, because we want to remove the max distance points
+            heapq.heappush(minHeap, (-distance, point))
+        while len(minHeap) > k:
+            heapq.heappop(minHeap)
+        output = []
+        for i in range(k):
+            output.append(heapq.heappop(minHeap)[1])
+        
+        return output
