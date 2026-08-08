96764bc9b638d5250261976f13cb33215a8b906c
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-08 17:25:19 -0400

    Add: find-pivot-index - submission-4

---
+++ Data Structures & Algorithms/find-pivot-index/submission-4.py
@@ -0,0 +1,18 @@
+#Algo
+#cant sort --> need index, and elements repeat too
+#brute force --> go through array each iteration to check sums O(n**2)
+
+#go through once to find full sum, just subtract from the left on a second pass O(n)
+#Time | Space
+#O(n) | O(1)
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        lsum, rsum = 0, sum(nums)
+
+        for pivot, num in enumerate(nums):
+            rsum -= num
+            if lsum == rsum:
+                return pivot
+            lsum += num
+
+        return -1
