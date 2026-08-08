dbbf17158158c11189c8464b7e3a8795f5a9822d
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-08 18:08:11 -0400

    Add: kth-distinct-string-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-2.py
@@ -0,0 +1,20 @@
+#Algo
+# go through arr, increment value if already seen
+# return kth key with value 1
+
+#Time | Space
+# O(n + m) | O(n), m number of distinct values in arr
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        distinct = defaultdict(str)
+        for string in arr:
+            if string in distinct.keys():
+                distinct[string]+=1
+            else:
+                distinct[string]=1
+        for string, count in distinct.items():
+            if count == 1:
+                k-=1
+            if k==0:
+                return string
+        return ""
