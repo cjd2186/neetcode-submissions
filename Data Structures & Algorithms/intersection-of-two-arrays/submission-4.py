e3b3f35a5bd5de846c3f6ad54cbdf05e598b4b1d
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-07 21:31:22 -0400

    Add: intersection-of-two-arrays - submission-4

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-4.py
@@ -0,0 +1,24 @@
+#O(n) --> two pointers
+
+#sort both arrays
+#iterate until you see they have common elements
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        n1, n2 = 0, 0
+        output = set()
+        print(nums1)
+        print(nums2)
+        while n1 < len(nums1) and n2 < len(nums2):
+            if nums1[n1] == nums2[n2]:
+                output.add(nums1[n1])
+                n1+=1
+                n2+=1
+            elif nums1[n1] > nums2[n2]:
+                n2+=1
+            elif nums2[n2] > nums1[n1]:
+                n1+=1
+            else:
+                n1+=1
+        return list(output)
