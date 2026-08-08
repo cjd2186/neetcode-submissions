de18bea315e19402c75a57f0abf3206e14a57eb3
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-08 13:02:10 -0400

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-5

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-5.py
@@ -0,0 +1,17 @@
+#naive?
+#track the frequency of every character in the string
+#output (max(odd) - min(even)) --> want max difference
+#Time: O(n) --> iterate through entirity of s
+#Space: O(1) -- dict will have at most 26 spots
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        charMap = defaultdict(str)
+        for char in s:
+            if char not in charMap:
+                charMap[char] = 0    
+            charMap[char] +=1
+        frequencies = list(charMap.values())
+        odd_freqs = [freq for freq in frequencies if freq%2==1]
+        even_freqs = [freq for freq in frequencies if freq%2==0]
+        return max(odd_freqs)-min(even_freqs)
+        
