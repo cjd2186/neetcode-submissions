909b405783e1bf8d62f1d38c788c00be433d95b3
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-09 14:52:16 -0400

    Add: find-all-numbers-disappeared-in-an-array - submission-5

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-5.py
@@ -0,0 +1,22 @@
+#Algo, sort, then remove dupes and have counter, if counter not in arr, add to output
+#remove dupes instead of set to ensure to extra space, remove operation is O(n) too
+
+#sort --> O(n log n) --> can use a dictionary instead?
+#for O(n) and no extra space would have to just go through the list once
+
+#one pass --> make output have indexes as values
+#second pass --> replace discovered indexes with 0
+#third pass --> return list without 0s
+#3*O(n) --> O(n) time --> but uses extra space
+
+#no extra space --> modify nums in place
+#make nums[abs(num)] negative
+#index of remaining positive nums are omitted from array
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for num in nums:
+            if nums[abs(num)-1] > 0:
+                nums[abs(num)-1] = -nums[abs(num)-1]
+                
+        #build output, add 1 to account for index change
+        return [i+1 for i, num in enumerate(nums) if num>0]
