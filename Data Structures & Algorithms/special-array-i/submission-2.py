56cf555839fea62e26b8ee1bf740766004cc9123
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-11 00:46:00 -0400

    Add: special-array-i - submission-2

---
+++ Data Structures & Algorithms/special-array-i/submission-2.py
@@ -0,0 +1,20 @@
+#Algo
+#use two pointers to go pairwise compare even/odd
+
+#Time --> O(n)
+#Space --> O(1)
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        l = 0
+        r = 1
+        while r < len(nums):
+            #one must be even, other odd
+            left = nums[l]
+            right = nums[r]
+            if left % 2 == 0 and right % 2 == 0:
+                return False
+            if left % 2 == 1 and right % 2 == 1:
+                return False
+            l+=1
+            r+=1
+        return True
