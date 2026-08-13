e31bf757c5691a7e7ef467646fefca64f97663d2
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-12 22:43:05 -0400

    Add: pascals-triangle-ii - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-1.py
@@ -0,0 +1,25 @@
+#Edges are always 1 and 1
+#can do it recursively?????
+
+#Time: O(n) --> go through n=rowIndex times
+#Space: O(n) --> will make n=rowIndex arrays
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        row = [1,1]
+        idx = 1
+        while idx < rowIndex:
+            row = self.recurse(row)
+            idx +=1
+        return row
+
+    def recurse(self, row) -> List[int]:
+        newRow=[1]
+        prev = row[0]
+        for i in range(1, len(row)):
+            curr = row[i]
+            newRow.append(prev+curr)
+            prev=curr
+        newRow.append(1)
+        return newRow
