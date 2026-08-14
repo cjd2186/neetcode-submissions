e45c959ff5dd6273504a833da5ebaedb730c88b9
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-13 23:23:21 -0400

    Add: maximum-product-difference-between-two-pairs - submission-6

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-6.py
@@ -0,0 +1,20 @@
+#want to find 2 max ints and 2 min ints --> 2 passes and 2 removes
+#Time: O(n) --  4 times
+#Space: O(1)
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        max1 = -1
+        max2 = -1
+        min1 = 100001
+        min2 = 100002
+
+        for num in nums:
+            max1 = max(max1, num)
+            min1 = min(min1, num)
+        nums.remove(max1)
+        nums.remove(min1)
+        for num in nums:
+            max2 = max(max2, num)
+            min2 = min(min2, num)
+    
+        return (max1*max2) - (min1*min2)
