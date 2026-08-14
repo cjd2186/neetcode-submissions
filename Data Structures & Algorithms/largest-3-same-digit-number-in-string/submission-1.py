a35edc6d77d69d82a1e1eaab91ca303d9072e652
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-13 22:33:47 -0400

    Add: largest-3-same-digit-number-in-string - submission-1

---
+++ Data Structures & Algorithms/largest-3-same-digit-number-in-string/submission-1.py
@@ -0,0 +1,19 @@
+#use sliding window to get substrings of length 3!
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+
+        maxGood = -1
+        l = 0
+        r = 2
+        while r < len(num):
+            window = num[l:r+1]
+            if (window[0] == window[1]) and (window[1] == window[2]):
+                maxGood = max(maxGood, int(window))
+            l+=1
+            r+=1
+        
+        if maxGood < 0:
+            return ""
+        elif maxGood == 0:
+            return "000"
+        return str(maxGood)
