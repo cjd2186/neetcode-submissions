c70d8f09c9a94fc8402f0caa1efcf03015cfccf9
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-14 11:15:32 -0400

    Add: path-crossing - submission-4

---
+++ Data Structures & Algorithms/path-crossing/submission-4.py
@@ -0,0 +1,30 @@
+#Algo
+#can keep track of all points traversed, if current point is in traversed, paths cross
+
+#Time: O(n) --> one pass, but search each element of pathes --optimize
+#Space: O(n) --> make a list for pathes, then a dict of the pathes later
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        pathes = ["00"]
+        curr=[0,0]
+        for move in path:
+            if move == 'N':
+                curr[0] +=1
+            elif move =='S':
+                curr[0] -=1
+            elif move == 'E':
+                curr[1] +=1
+            elif move == 'W':
+                curr[1] -=1
+            str_curr = str(curr[0]) + str(curr[1])
+            pathes.append(str_curr)
+        pathCounts = defaultdict(int)
+        for path in pathes:
+            if pathCounts[path] == None:
+                pathCounts[path] = 0
+            else:
+                pathCounts[path] += 1
+        for path in pathCounts.values():
+            if path > 1:
+                return True
+        return False
