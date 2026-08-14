92a31f21686e0007fbeb0d81f40fdc42c1d408c8
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-13 21:41:19 -0400

    Add: count-the-number-of-consistent-strings - submission-1

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-1.py
@@ -0,0 +1,15 @@
+#Time: O() --> 
+#Space: O(1) --> unqiueChars is at most length 26
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        output = 0
+        for word in words:
+            uniqueChars = list(Counter(word).keys())
+            good = True
+            for char in uniqueChars:
+                if char not in allowed:
+                    good = False
+                    break
+            if good:
+                output +=1
+        return output
