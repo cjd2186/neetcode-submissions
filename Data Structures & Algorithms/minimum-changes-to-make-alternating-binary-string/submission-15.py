6b63aff6caf66a6d4697581379437c2dd70d142a
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-18 01:09:55 -0400

    Add: minimum-changes-to-make-alternating-binary-string - submission-15

---
+++ Data Structures & Algorithms/minimum-changes-to-make-alternating-binary-string/submission-15.py
@@ -0,0 +1,21 @@
+#Algo
+
+#Two patterns , 01 10
+#try both and return min
+class Solution:
+    def minOperations(self, s: str) -> int:
+        pattern0=0
+        for i in range(len(s)):
+            if i%2==0 and s[i] != '0':
+                pattern0 +=1
+            elif i%2==1 and s[i] != '1':
+                pattern0 +=1 
+        
+        pattern1=0
+        for i in range(len(s)):
+            if i%2==0 and s[i] != '1':
+                pattern1 +=1
+            elif i%2==1 and s[i] != '0':
+                pattern1 +=1
+
+        return min(pattern0, pattern1)
