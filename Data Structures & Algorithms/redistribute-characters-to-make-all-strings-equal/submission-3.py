42b005d8a4ac2f9af78faa6142a61d60b50b2919
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-18 01:33:02 -0400

    Add: redistribute-characters-to-make-all-strings-equal - submission-3

---
+++ Data Structures & Algorithms/redistribute-characters-to-make-all-strings-equal/submission-3.py
@@ -0,0 +1,17 @@
+#Time: O(n) -- counter O(n), checking is O(n)
+#Space: O(1) --> counter has at most 26 spaces
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        char_string = "".join(words)
+        char_count = Counter(char_string)
+        needed_count = len(words)
+        for count in char_count.values():
+            if count % needed_count != 0:
+                return False
+        return True
+
+        
+# aaabc, abc, bc, bc
+#remove common chars from all words
+#if number of leftover chars = #words true, otw false
+#need count of each char to be equal to #words
