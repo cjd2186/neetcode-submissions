c76d5bbe7757980cd1d255bf0e62caed44ea1dbf
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-08-18 21:30:56 -0400

    Add: find-common-characters - submission-5

---
+++ Data Structures & Algorithms/find-common-characters/submission-5.py
@@ -0,0 +1,23 @@
+#Algo --> hashmap, find char frequency for each word, compare freqs
+#Time O(m), length of all words * O(n) number of words --> each character is counted once
+#Space: O(1) --> 52 letters, 26 in each word counter at most
+
+#get frequency of each word/letter take the min
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        wordFreqs = Counter(words[0])
+        
+        for word in words:
+            letterCount = Counter(word)
+            for letter in wordFreqs:
+                if letter in letterCount:
+                    wordFreqs[letter] = min(wordFreqs[letter], letterCount[letter])
+                else:
+                    wordFreqs[letter] = 0
+        output = []
+        
+        for letter, freq in wordFreqs.items():
+            for i in range(freq):
+                output.append(letter)
+        return output
