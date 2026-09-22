a004adf8f9e581f7117d2ecebcf33d5c8402515c
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2026-09-21 23:28:40 -0400

    Add: sort-array-by-increasing-frequency - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-0.py
@@ -0,0 +1,14 @@
+#get counts using counter and sort array
+#counter puts most most frequency at the beginning but doenst do the desc order
+#if tie, sort nums in decreasing order
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freqs = Counter(nums)
+        #make sublist when freqs are the same and just sort that sublist in desc?
+        freqs = sorted(freqs.items(), key=lambda item: (item[1], -item[0]))
+        output = []
+        for (num, freq) in freqs:
+            for i in range(freq):
+                output.append(num)
+        
+        return output
